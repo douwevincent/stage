f52d72611554262a4cd0d5895dbc8b43493d60c3
Vincent Douwe <douwevincent@yahoo.fr>
2026-02-25 16:02:14 +0100

    Modification du projet en un WAR

--- src/main/java/cm/univ/maroua/enspm/stage/StageApplication.java
+++ src/main/java/cm/univ/maroua/enspm/stage/StageApplication.java
@@ -2,9 +2,16 @@ package cm.univ.maroua.enspm.stage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StageApplication {
+public class StageApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StageApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StageApplication.class, args);
