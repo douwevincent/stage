f3ca80c7503b02b5e6255fa16098fcbdf7054c0f
Vincent Douwe <douwevincent@yahoo.fr>
2026-04-02 12:55:57 +0100

    Deseactivation temporaire de la sécurité

--- src/main/java/cm/univ/maroua/enspm/stage/config/SecurityConfig.java
+++ src/main/java/cm/univ/maroua/enspm/stage/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/stages/declarer").permitAll()
                         .requestMatchers("/api/v1/users/**").hasRole("SUPER_ADMIN")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
